43e525e10a804cc21d41c61c5cc6c9c7d4f2375e
Matvei <matveiyan@yandex.ru>
2026-05-09 13:46:13 +0300

    update public commands

--- lib/routers/public_commands.py
+++ lib/routers/public_commands.py
@@ -6,9 +6,13 @@ router = Router()
 
 @router.message(Command("chat_id"))
 async def chat_id_cmd(message: types.Message):
-    await message.answer(str(message.chat.id))
+    return await message.reply(str(message.chat.id))
 
 
 @router.message(Command("user_id"))
 async def user_id_cmd(message: types.Message):
-    await message.answer(str(message.from_user.id))
+    if message.reply_to_message:
+        return await message.reply(
+            f'{message.reply_to_message.from_user.username}: {message.reply_to_message.from_user.id}'
+        )
+    return await message.reply(f'{message.from_user.username}: {message.from_user.id}')
